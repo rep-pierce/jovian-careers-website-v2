57c3472114ce5e19af8acf5655ca831c79274d2e
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-05-02 14:28:11 -0600

    created submitted app page with dynamic info

--- app.py
+++ app.py
@@ -1,36 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
-# JOBS = [
-#     {
-#         'id': 1,
-#         'title': 'Data Analyst',
-#         'location': 'Bengaluru, India',
-#         'salary': 'USD 70,000'
-#     },
-#     {
-#         'id': 2,
-#         'title': 'Front-end Engineer',
-#         'location': 'Pleasant Grove, UT',
-#         'salary': 'USD 75,000'
-#     },
-#     {
-#         'id': 3,
-#         'title': 'Back-end Engineer',
-#         'location': 'Phoenix, AZ',
-#         'salary': 'USD 80,000'
-#     },
-#     {
-#         'id': 4,
-#         'title': 'Full-stack Engineer',
-#         'location': 'Portland, OR',
-#         'salary': 'USD 85,000'
-#     }
-# ]
-
-
 
 @app.route("/")
 def hello_world():
@@ -49,6 +20,14 @@ def show_job(id):
     if not job:
         return "Not Found", 404
     return render_template('jobpage.html',
+                           job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    return render_template('application_submitted.html',
+                           application=data,
                            job=job)
 
 if __name__ == "__main__":
